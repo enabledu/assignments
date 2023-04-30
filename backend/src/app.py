c7f3d6f3c3962ad988c0916c98b05388eab7aca3
MWah99d <mwpqnu@gmail.com>
2023-04-30 18:23:47 +0200

    Add the remaining endpoints

--- backend/src/app.py
+++ backend/src/app.py
@@ -28,23 +28,21 @@ async def get_assignment(assignment_id: UUID = None):
 async def add_assignment(title: str = Body(),
                          deadline: datetime = Body(None),
                          description: str = Body(None),
-                         attachments: list[UploadFile] = Body(None),
                          max_grade: int = Body(None)):
     pass
 
 
-@app.post("/assignment/{assignment_id}/edit/")
+@app.post("/assignment/{assignment_id}/update/")
 async def update_assignment(assignment_id: UUID,
                             title: str = Body(),
                             description: str = Body(None),
-                            attachments: list[UploadFile] = Body(None),
                             max_grade: int = Body(None)):
     pass
     # check if the id is valid
 
 
 @app.delete("/assignment/{assignment_id}/delete/")
-async def remove_assignment(assignment_id: UUID):
+async def delete_assignment(assignment_id: UUID):
     pass
     # check if the id is valid
 
@@ -61,15 +59,88 @@ async def submit_work(assignment_id: UUID):
     # check if the id is valid
 
 
-@app.get("/assignment/{assignment_id}/attachment")
-async def get_attachment(assignment_id: UUID):
+@app.get("/assignment/{assignment_id}/attachment/")
+async def get_all_assignment_attachments(assignment_id: UUID):
     pass
     # check if the id is valid
 
 
+@app.get("/assignment/attachment/{attachment_id}/")
+async def get_assignment_attachment(attachment_id: UUID):
+    pass
+    # check if the id is valid
 
 
+@app.post("/assignment/{assignment_id}/attachment/add/")
+async def add_assignment_attachment(assignment_id: UUID):
+    pass
+    # check if the id is valid
 
 
+@app.delete("/assignment/attachment/{attachment_id}/delete/")
+async def delete_assignment_attachment(attachment_id: UUID):
+    pass
+    # check if the id is valid
+
+
+@app.get("/work/{work_id}/")
+async def get_work(work_id: UUID):
+    pass
+    # check if the id is valid
+
+
+@app.get("/work/{work_id}/attachment/")
+async def get_all_work_attachments(work_id: UUID):
+    pass
+    # check if the id is valid
+
+
+@app.get("/work/attachment/{attachment_id}/")
+async def get_all_work_attachments(attachment_id: UUID):
+    pass
+    # check if the id is valid
+
+
+@app.post("/work/{work_id}/unsubmit/")
+async def unsubmit_work(work_id: UUID):
+    pass
+    # check if the id is valid
+
+
+@app.post("/work/{work_id}/attachment/add/")
+async def add_work_attachment(work_id: UUID):
+    pass
+    # check if the id is valid
+
+
+@app.delete("/work/attachment/{attachment_id}/delete/")
+async def delete_attachment(attachment_id: UUID):
+    pass
+    # check if the id is valid
+
+
+@app.post("/work/{work_id}/grade/add/")
+async def add_work_grade(work_id: UUID):
+    pass
+    # check if the id is valid
+
+
+@app.post("/work/{work_id}/grade/update/")
+async def update_work_grade(work_id: UUID):
+    pass
+    # check if the id is valid
+
+
+# Other way for attachment implementation
+@app.get("attachment/{attachment_id}/")
+async def get_attachment(attachment_id: UUID):
+    pass
+    # check if the id is valid
+
+
+@app.delete("attachment/{attachment_id}/delete")
+async def delete_attachment(attachment_id: UUID):
+    pass
+    # check if the id is valid
 
 
