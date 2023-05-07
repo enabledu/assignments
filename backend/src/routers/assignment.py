cae303c3d36281e8c6e24a85fc17ae56816982f5
mabagoury <wnajsksos@gmail.com>
2023-05-07 20:01:42 +0300

    fix endpoints prefixes correctly

--- backend/src/routers/assignment.py
+++ backend/src/routers/assignment.py
@@ -9,18 +9,18 @@ from assignments.backend.src import queries
 assignment_router = APIRouter(prefix="/assignment")
 
 
-@assignment_router.get("/assignment/")
+@assignment_router.get("/")
 async def get_all_assignments(client=Depends(get_client)) -> list[Assignment]:
     return await queries.get_all_assignments(client)
 
 
-@assignment_router.post("/assignment/add/")
+@assignment_router.post("/add/")
 async def add_assignment(assignment: BaseAssignment,
                          client=Depends(get_client)) -> UUID:
     return await queries.add_assignment(client, **BaseAssignment.dict())
 
 
-@assignment_router.post("/assignment/{assignment_id}/edit/")
+@assignment_router.post("/{assignment_id}/edit/")
 async def edit_assignment(assignment_id: UUID,
                           assignment: BaseAssignment,
                           client=Depends(get_client)) -> None:
@@ -28,7 +28,7 @@ async def edit_assignment(assignment_id: UUID,
     # check if the id is valid
 
 
-@assignment_router.delete("/assignment/{assignment_id}/delete/")
+@assignment_router.delete("/{assignment_id}/delete/")
 async def delete_assignment(assignment_id: UUID,
                             client=Depends(get_client)) -> None:
     pass
@@ -37,14 +37,14 @@ async def delete_assignment(assignment_id: UUID,
     #         await tx.execute("INSERT User {name := 'Don'}")
 
 
-@assignment_router.get("/assignment/{assignment_id}/attachments/")
+@assignment_router.get("/{assignment_id}/attachments/")
 async def get_all_assignment_attachments(assignment_id: UUID,
                                          client=Depends(get_client)) -> list[Attachment]:
     pass
     # check if the id is valid
 
 
-@assignment_router.post("assignment/{assignment_id}/attachment/add")
+@assignment_router.post("/{assignment_id}/attachment/add/")
 async def add_attachment_to_assignment(assignment_id: UUID,
                                        attachment: UploadFile,
                                        client=Depends(get_client)) -> UUID:
@@ -52,14 +52,14 @@ async def add_attachment_to_assignment(assignment_id: UUID,
     # check if the id is valid
 
 
-@assignment_router.get("/assignment/{assignment_id}/works/")
+@assignment_router.get("/{assignment_id}/works/")
 async def get_all_assignment_works(assignment_id: UUID,
                                    client=Depends(get_client)) -> list[Work]:
     pass
     # check if the id is valid
 
 
-@assignment_router.post("assignment/{assignment_id}/work/add")
+@assignment_router.post("/{assignment_id}/work/add/")
 async def add_attachment_to_work_on_assignment(assignment_id: UUID,
                                                attachment: UploadFile,
                                                client=Depends(get_client)) -> UUID:
--- backend/src/routers/attachment.py
+++ backend/src/routers/attachment.py
@@ -8,14 +8,14 @@ from assignments.backend.src import queries
 attachment_router = APIRouter(prefix="/attachment")
 
 
-@attachment_router.get("attachment/{attachment_id}/")
+@attachment_router.get("/{attachment_id}/")
 async def get_attachment(attachment_id: UUID,
                          client=Depends(get_client)) -> bytes:
     pass
     # check if the id is valid
 
 
-@attachment_router.delete("attachment/{attachment_id}/delete")
+@attachment_router.delete("/{attachment_id}/delete")
 async def delete_attachment(attachment_id: UUID,
                             client=Depends(get_client)) -> None:
     pass
